4a5ec009da4ae753e7b175c48cc307e5a299c97e
arule11 <47372331+arule11@users.noreply.github.com>
2021-03-27 15:29:14 -0600

    Update Furniture.java
    output methods

--- edu/ucalgary/ensf409/Furniture.java
+++ edu/ucalgary/ensf409/Furniture.java
@@ -3,117 +3,38 @@ package edu.ucalgary.ensf409;
 
 public class Furniture{
 
-  public final String DBURL; //store the database url information
-  public final String USERNAME; //store the user's account username
-  public final String PASSWORD; //store the user's account password
-
-  private Connection dbConnect;
-  private ResultSet results;
-
-
-  public Registration(String url, String username, String pswd){
-    this.DBURL = url;
-    this.USERNAME = username;
-    this.PASSWORD = pswd;
-  }
-
-  public void initializeConnection(){
-      try{
-        dbConnect = DriverManager.getConnection(getDburl(), getUsername(), getPassword());
-      } catch (SQLException e) {
-          e.printStackTrace();
-      }
-  }
+  public void printOutput(){
+    StringBuffer names = new StringBuffer();
+    try{
+      Statement myStmt = dbConnect.createStatement();
+      results = myStmt.executeQuery("SELECT * FROM MANUFACTURER");
   
-  /**
-  * Retrieves the database URL
-  * @return Returns a String
-  */
-  public String getDburl(){
-    return this.DBURL;
-  }
-
-  /**
-  * Retrieves the MySQL username
-  * @return Returns a String
-  */
-  public String getUsername(){
-    return this.USERNAME;
-  }
-
-  /**
-  * Retrieves the MySQL password
-  * @return Returns a String
-  */
-  public String getPassword(){
-    return this.PASSWORD;
-  }
-
-
-checkChair(String type){
-  try{
-    Statement myStmt = dbConnect.createStatement();
-    results = myStmt.executeQuery("SELECT * FROM CHAIR WHERE type = '" + type + "'");
-
-    ArrayList<String> chairs = new ArrayList<String>();
-
-    while (results.next()){
-      // if(results.getString("Legs").equals("Y")){
-      // results.getString("ID");
-      // }
-
+      while (results.next()){
+        if(results.isLast()){
+          names.append("and "+ results.getString("Name"));
+        }else{
+          names.append(results.getString("Name") + ", " );
+        }
+      }
+      myStmt.close();
+    } catch(SQLException ex) {
+        ex.printStackTrace();
     }
-    ArrayList<String> chairs = new ArrayList<String>();
-    StringBuffer row = new StringBuffer();
-    row.append(results.getString("ID") + "=" + results.getString("Legs") + "," + results.getString("Arms") + "," + results.getString("Seat") + "," + results.getString("Cushion"));
-
-
-  }catch (SQLException ex){
-    ex.printStackTrace();
+    System.out.println("Order cannot be fulfilled based on current inventory. Suggested manufacturers are "
+            + names.toString());
   }
-}
 
-checkDesk(String type){
-  try{
-    Statement myStmt = dbConnect.createStatement();
-    results = myStmt.executeQuery("SELECT * FROM DESK WHERE type = '" + type + "'");
-
-    if(!results.next()){
-      throw new IllegalArgumentException();
+  public static void printOutput(String[] ids, String price){
+    System.out.print("Purchase ");
+    for(int i = 0; i < ids.length; i++){
+      if(i == ids.length - 1){
+        System.out.print("and " + ids[i] );
+      }else{
+        System.out.print(ids[0] + " ");
+      }
     }
-  }catch (SQLException ex){
-    ex.printStackTrace();
+    System.out.println(" for " + price);
   }
-}
-
-
-checkFiling(String type){
-  try{
-    Statement myStmt = dbConnect.createStatement();
-    results = myStmt.executeQuery("SELECT * FROM FILING WHERE type = '" + type + "'");
-
-    if(!results.next()){
-      throw new IllegalArgumentException();
-    }
-  }catch (SQLException ex){
-    ex.printStackTrace();
-  }
-
-}
-
-checkLamp(String type){
-  try{
-    Statement myStmt = dbConnect.createStatement();
-    results = myStmt.executeQuery("SELECT * FROM LAMP WHERE type = '" + type + "'");
-
-    if(!results.next()){
-      throw new IllegalArgumentException();
-    }
-  }catch (SQLException ex){
-    ex.printStackTrace();
-  }
-
-}
 
 
 }
