04ca63f3aa50a30adc2f155ec4e48395c9fd2948
arule11 <47372331+arule11@users.noreply.github.com>
2021-03-27 15:36:54 -0600

    Update Furniture.java

--- edu/ucalgary/ensf409/Furniture.java
+++ edu/ucalgary/ensf409/Furniture.java
@@ -2,6 +2,38 @@ package edu.ucalgary.ensf409;
 
 
 public class Furniture{
+  
+  public static void WriteOrderForm(String orgOrder, String[] ids, String price){
+    String outputFile = "orderform.txt";
+    File toWrite = new File(outputFile);
+    try{
+      if(toWrite.exists() && toWrite.isFile()){
+        toWrite.delete();
+      }
+      toWrite.createNewFile();
+
+      FileWriter writer = new FileWriter(outputFile);
+      BufferedWriter out = new BufferedWriter(writer);
+      //out.write();
+      out.write("Furniture Order Form \n\n");
+      out.write("Faculty Name:\n");
+      out.write("Contact:\n");
+      out.write("Date:\n\n");
+
+      out.write("Original Request: " + orgOrder + "\n\n");
+      out.write("Items Ordered");
+      for(String i : ids){
+        out.write("\nID: " + i);
+      }
+      out.write("\n\nTotal Price: " + price);
+
+      out.flush();
+      out.close();
+    }catch(Exception e){
+      System.out.println(e);
+      System.exit(0);
+    }
+  }
 
   public void printOutput(){
     StringBuffer names = new StringBuffer();
