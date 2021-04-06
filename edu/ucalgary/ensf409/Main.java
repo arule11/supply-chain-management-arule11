e087d7ea9f93ef19523d82058ee335827e7b6bf9
arule11 <47372331+arule11@users.noreply.github.com>
2021-04-05 19:53:24 -0600

    Update Main.java

--- edu/ucalgary/ensf409/Main.java
+++ edu/ucalgary/ensf409/Main.java
@@ -1,3 +1,12 @@
+/**
+@author   Athena McNeil-Roberts : athena.mcneilrobe1@ucalgary.ca
+          Nicolas Teng : 
+          Ivan Lou Tompong :
+          Alden Lien : 
+@version 1.3
+@since 1.0
+*/
+
 package edu.ucalgary.ensf409;
 
 import java.util.regex.*;
@@ -9,20 +18,20 @@ public class Main{
       System.out.println("Usage: Main <category> <furniture> <amount>");
       System.exit(1);
     }
+    
     String type = createArg(args[1]);
 
     FurnitureDataBase item = new FurnitureDataBase(args[0], type, Integer.parseInt(args[2]));
     item.initializeConnection();
     item.addFurniture();
     item.run();
-
-    //System.out.println();
-    // FurnitureDataBase item2 = new FurnitureDataBase("Standing", "Desk", 2);
-    // item2.initializeConnection();
-    // item2.addFurniture();
-    // item2.run();
   }
 
+  /**
+  * Crops the argument of furniture type to just the name not including any commas
+  * @param type : the specified argument to crop 
+  * @return Returns a String
+  */
   public static String createArg(String type){
     Pattern pattern = Pattern.compile("[a-zA-Z]{4,6}");
     Matcher matcher = pattern.matcher(type);
