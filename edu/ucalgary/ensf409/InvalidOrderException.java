3b96327f5ec2ff41de8da53b69270cc76264e0ff
arule11 <47372331+arule11@users.noreply.github.com>
2021-04-08 15:39:07 -0600

    Update InvalidOrderException.java

--- edu/ucalgary/ensf409/InvalidOrderException.java
+++ edu/ucalgary/ensf409/InvalidOrderException.java
@@ -1,3 +1,12 @@
+/**
+@authors  Athena McNeil-Roberts : athena.mcneilrobe1@ucalgary.ca
+          Nicolas Teng :
+          Ivan Lou Tompong : ivanlou.tompong@ucalgary.ca
+          Alden Lien :
+@version 1.1
+@since 1.0
+*/
+
 package edu.ucalgary.ensf409;
 /**
 * Class representing a InvalidOrderException
