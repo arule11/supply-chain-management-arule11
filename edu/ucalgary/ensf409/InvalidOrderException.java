5f9ad4c642206f1ed9c5f168a6a63c52ae51eb89
aldenlien <81485740+aldenlien@users.noreply.github.com>
2021-04-08 18:58:14 -0600

    Update InvalidOrderException.java

--- edu/ucalgary/ensf409/InvalidOrderException.java
+++ edu/ucalgary/ensf409/InvalidOrderException.java
@@ -2,7 +2,7 @@
 @author  Athena McNeil-Roberts : athena.mcneilrobe1@ucalgary.ca
           Nicolas Teng : nicolas.teng@ucalgary.ca
           Ivan Lou Tompong : ivanlou.tompong@ucalgary.ca
-          Alden Lien :
+          Alden Lien : alden.lien@ucalgary.ca
 @version 1.1
 @since 1.0
 */
